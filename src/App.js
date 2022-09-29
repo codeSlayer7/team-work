5eb3e71271872e1256c25736e3824615454a3bcf
codeSlayer7 <bahazve01@gmail.com>
2022-09-29 15:21:25 +0600

    add redux-toolkit and grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Box } from '@mui/material';
 import './App.css';
+import CardsBox from './component/cards/cardsSection';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{
+      backgroundColor:'#FFFFFF',
+      maxWidth: 1000,
+      margin: 'auto'
+    }}>
+
+      <CardsBox/>
+     
+    
+    </Box>
   );
 }
 
